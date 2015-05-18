677e281f5cea01399af80f089cad74a0ed6b1536
gaurang <gaurangkher@yahoo.com>
2015-05-18 07:46:16 -0500

    rename app name to Vartaa

--- app/models/vartaa.rb
+++
@@ -1,2 +0,0 @@
-class Vartaa < ActiveRecord::Base
-end
--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Final
+module Vartaa
   class Application < Rails::Application
     config.action_controller.permit_all_parameters = true
   end
