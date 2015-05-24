a2a9b560969eb91e6f4f160f4c68e76aec54a528
gaurang <gaurangkher@yahoo.com>
2015-05-23 23:43:30 -0500

    add categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :article
   #get '/hello/index', to: 'hello#index'
+  get '/politics', to: 'article#politics'
+  get '/oped', to: 'article#oped'
+  get '/business', to: 'article#business'
+  get '/sports', to: 'article#sports'
+  get '/ent', to: 'article#ent'
 
   root to: "article#index"
 end
