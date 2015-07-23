2090c4414855c5155ffff2fa8844397847f5f415
nkmilind <nk.milind@gmail.com>
2015-07-22 19:32:40 -0500

    Made sign-on branch up to date
    Made sign-on branch up to date

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
 
   resources :article
+  resources :users
+  resources :sessions
   #get '/hello/index', to: 'hello#index'
   get '/politics', to: 'article#politics'
   get '/oped', to: 'article#oped'
   get '/business', to: 'article#business'
   get '/sports', to: 'article#sports'
   get '/ent', to: 'article#ent'
+  get "/logout", to: "sessions#destroy"
 
   root to: "article#index"
 end
