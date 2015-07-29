554858ed4a6fa35f88285105625e1b467456b838
nkmilind <nk.milind@gmail.com>
2015-07-29 12:01:06 -0500

    Added 'validates_uniqueness_of :email'
    Added 'validates_uniqueness_of :email' to ensure users do not sign up
    with same email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
+  validates_uniqueness_of :email
   has_secure_password
 end
