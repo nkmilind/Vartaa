5f5d6184d7a1ace23dd93bdf7b709b3574e226f7
gaurang <gaurangkher@yahoo.com>
2015-08-16 21:14:34 -0500

    add sort by time and rank in search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,20 @@ class SearchController < ApplicationController
             @articles = []
         else
             
-            @articles = Article.search(query: {simple_query_string: {  query: params[:q], analyzer: 'snowball', fields: [ '_all'] , default_operator: 'or'} } ).paginate(page: params['page'] || 1, per_page: 20)
+            @articles = Article.search(
+                query: {
+                    simple_query_string: {  
+                        query: params[:q], 
+                        analyzer: 'snowball', 
+                        fields: [ '_all'] , 
+                        default_operator: 'or'
+                    } 
+                },
+                sort: {
+                    date:   { "order": "desc" },
+                    _score: { "order": "desc" }
+                } 
+            ).paginate(page: params['page'] || 1, per_page: 20)
         end
     end
 end
