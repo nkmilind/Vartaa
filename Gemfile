b5ba2faf83429b45fe37760fdf9376489ddc74ae
gaurang <gaurangkher@yahoo.com>
2015-08-16 20:46:05 -0500

    some more changes to search

--- Gemfile
+++ Gemfile
@@ -25,14 +25,14 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-# Elasticsearch gems
-gem 'elasticsearch-model'
-gem 'elasticsearch-rails'
-
 gem 'htmlentities'
 gem 'will_paginate-bootstrap', '~> 1.0.1'
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
+
+# Elasticsearch gems, needs to be after will_paginate declaration
+gem 'elasticsearch-model'
+gem 'elasticsearch-rails'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,9 +4,8 @@ class SearchController < ApplicationController
         if params[:q].nil?
             @articles = []
         else
-            @articles = Article.search query:     { match:  { title: "Modi" } }
-            logger.info @articles
-            #@articles = Article.search params[:q]
+            
+            @articles = Article.search(query: {simple_query_string: {  query: params[:q], analyzer: 'snowball', fields: [ '_all'] , default_operator: 'or'} } ).paginate(page: params['page'] || 1, per_page: 20)
         end
     end
 end
