f8c05de8974eabb1ef191e0a8044e4663cff8605
gaurang <gaurangkher@yahoo.com>
2015-08-21 00:33:22 -0500

    fix latest headlines section

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -1,6 +1,12 @@
 class ArticleController < ApplicationController
   skip_before_filter :auth, only: [:show, :index, :politics, :business, :oped, :sports, :ent]
   def index
+    @latest = Article.select('title').where(
+        "date >= ? AND category_id not in (?,?,?,?,?,?,?)", 
+        Date.today.prev_day.to_formatted_s(:db),
+        3, 4, 5, 6, 62, 115, 117 
+    ).joins(' JOIN ranking on ranking.id = article.id').order('ranking.page_rank').limit(50) 
+
     @article = Article.select('*, ranking.admin, ranking.likes, ranking.dislikes').where(
         "date >= ? AND category_id not in (?,?,?,?,?,?,?)", 
         Date.today.prev_day.to_formatted_s(:db),
